349cf4cc7331cf0311aea325b89a5073fafbc09e
Andr? Furchner <storkner@googlemail.com>
2014-04-16 11:49:09 -0700

    QtQuickGL: Added methods to class drawable

--- QtQuickGL/src/render/object/drawable.cpp
+++ QtQuickGL/src/render/object/drawable.cpp
@@ -1,5 +1,26 @@
 #include "drawable.h"
 
-Drawable::Drawable()
+Drawable::Drawable(QObject* parent)
 {
+    m_vao = new QOpenGLVertexArrayObject(parent);
+    m_vao->create();
+
+    m_colorBuffer = NULL;
+    m_vertexBuffer = NULL;
+    m_indexBuffer = NULL;
+}
+
+void Drawable::SetVertices(float *vertices, int size_t)
+{
+
+}
+
+void Drawable::SetColors(float *colors, int size_t)
+{
+
+}
+
+void Drawable::SetIndices(int *indices, int size_t)
+{
+
 }
--- QtQuickGL/src/render/object/drawable.h
+++ QtQuickGL/src/render/object/drawable.h
@@ -1,12 +1,26 @@
 #ifndef DRAWABLE_H
 #define DRAWABLE_H
 
+#include <QOpenGLVertexArrayObject>
+#include <QOpenGLBuffer>
+
 class Drawable
 {
 public:
-    Drawable();
+    Drawable(QObject *parent);
 
     void Draw();
+
+    void SetVertices(float* vertices, int size_t);
+    void SetColors(float* colors, int size_t);
+    void SetIndices(int* indices, int size_t);
+
+private:
+    QOpenGLVertexArrayObject* m_vao;
+
+    QOpenGLBuffer* m_vertexBuffer;
+    QOpenGLBuffer* m_colorBuffer;
+    QOpenGLBuffer* m_indexBuffer;
 };
 
 #endif // DRAWABLE_H
