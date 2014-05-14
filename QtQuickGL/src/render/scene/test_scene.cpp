8e64962a13c012f9f40c7bad5eee0a93be96d297
Andr? Furchner <storkner@googlemail.com>
2014-05-14 07:37:49 +0200

    new test scene

--- QtQuickGL/src/render/scene/test_scene.cpp
+++ QtQuickGL/src/render/scene/test_scene.cpp
@@ -15,7 +15,7 @@ TestScene::TestScene(RenderEngine *engine) : Scene(engine)
 
 void TestScene::Create()
 {
-    m_d1 = m_factory->GenRectangle(10.0f,10.0f,QVector4D(0.25,1.0,0.0,1.0),m_engine->GetShader(QString("basic")));
+    m_d1 = m_factory->GenRectangle(10.0f,10.0f,QVector4D(1.0,1.0,1.0,1.0),m_engine->GetShader(QString("basic")));
 
 
     for(int i = 0; i < 10; ++i) {
@@ -23,13 +23,21 @@ void TestScene::Create()
         float g = (float)(qrand() % 255) / 255;
         float b = (float)(qrand() % 255) / 255;
 
-        Drawable* d2 = m_factory->GenRectangle(10.0f,10.0f,QVector4D(r,g,b,1.0),m_engine->GetShader(QString("basic")));
+        Drawable* d2 = m_factory->GenRectangle(7.5f,7.5f,QVector4D(r,g,b,1.0),m_engine->GetShader(QString("basic")));
 
-        QMatrix4x4 *q = new QMatrix4x4();
-        q->setToIdentity();
-        q->rotate((float)i * 360.0 / 10.0,0.0f,0.0f,1.0f);
-        q->translate(20.0f, 0.0f);
-        m_d1->AddChild(d2,q);
+        QMatrix4x4 *m1 = new QMatrix4x4();
+        m1->setToIdentity();
+        m1->rotate((float)i * 360.0 / 10.0,0.0f,0.0f,1.0f);
+        m1->translate(20.0f, 0.0f);
+        m_d1->AddChild(d2,m1);
+        for(int j = 0; j < 5; ++j) {
+            Drawable* d3 = m_factory->GenRectangle(2.5f,2.5f,QVector4D(r,g,b,1.0),m_engine->GetShader(QString("basic")));
+            QMatrix4x4 *m2 = new QMatrix4x4();
+            m2->setToIdentity();
+            m2->rotate((float)j * 360.0 / 5.0,0.0f,0.0f,1.0f);
+            m2->translate(10.0f, 0.0f);
+            d2->AddChild(d3,m2);
+        }
     }
 
     m_engine->AddDrawable(m_d1);
@@ -44,9 +52,15 @@ void TestScene::Update()
 {
     m_d1->GetTransformMatrix()->rotate(-2.0f,0.0f,0.0f,1.0f);
 
+
     int counti = m_d1->GetChildCount();
     for(int i = 0; i < counti; ++i) {
-       m_d1->GetChild(i)->GetTransformMatrix()->rotate(-0.5f,0.0f,0.0f,10.0f);
+       m_d1->GetChild(i)->GetTransformMatrix()->rotate(-2.0f,0.0f,0.0f,10.0f);
+
+       int countj = m_d1->GetChild(i)->GetChildCount();
+       for(int j = 0; j < countj; ++j) {
+           m_d1->GetChild(i)->GetChild(j)->GetTransformMatrix()->rotate(-2.0f,0.0f,0.0f,10.0f);
+       }
     }
 }
 
