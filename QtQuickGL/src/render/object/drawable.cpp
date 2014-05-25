0b2620b93c1dbb1abf8341590acaa9282acea8ac
Foxel <Foxel@localhost>
2014-05-25 16:07:21 +0200

    Comments for class Drawable

--- QtQuickGL/src/render/object/drawable.cpp
+++ QtQuickGL/src/render/object/drawable.cpp
@@ -15,28 +15,29 @@ Drawable::Drawable(RenderEngine *engine, QMatrix4x4 *transform)
     m_shader = NULL;
     m_mesh = NULL;
     m_modelMatrix = NULL;
-    m_indexCount = 0;
-    m_childCount = 0;
     m_engine = engine;
     m_transMatrix = transform;
 }
 
 void Drawable::Draw(QMatrix4x4 *transform)
 {
+    // bind my shader to be used
     m_shader->bind();
 
-    // set uniform color
+    // set my uniform color
     m_shader->setUniformValue("color", m_color);
 
-    // calculate nad set uniform variable modelMatrix
+    // calculate and set uniform variable modelMatrix
     QMatrix4x4 t2 = *transform * *m_transMatrix;
     m_shader->setUniformValue("modelMatrix", t2 * *m_modelMatrix);
 
+    // draw myself
     m_mesh->Draw();
     m_shader->release();
 
-    for(unsigned int i = 0; i < m_container.size(); ++i) {
-        m_container[i]->Draw(&t2);
+    // draw my childs
+    for(unsigned int i = 0; i < m_childList.size(); ++i) {
+        m_childList[i]->Draw(&t2);
     }
 }
 
@@ -87,8 +88,7 @@ void Drawable::SetTransformMatrix(QMatrix4x4 *matrix)
 void Drawable::AddChild(Drawable *child, QMatrix4x4 *transform)
 {
     child->m_transMatrix = transform;
-    m_container.push_back(child);
-    ++m_childCount;
+    m_childList.push_back(child);
 }
 
 void Drawable::SetColor(QVector4D color)
@@ -98,12 +98,12 @@ void Drawable::SetColor(QVector4D color)
 
 Drawable *Drawable::GetChild(int index)
 {
-    return m_container[index];
+    return m_childList[index];
 }
 
 int Drawable::GetChildCount()
 {
-    return m_childCount;
+    return m_childList.size();
 }
 
 QMatrix4x4 *Drawable::GetTransformMatrix()
--- QtQuickGL/src/render/object/drawable.h
+++ QtQuickGL/src/render/object/drawable.h
@@ -5,50 +5,105 @@
 #include <QVector4D>    // for color
 
 // Forward Declaration
-class QOpenGLVertexArrayObject;
-class QOpenGLBuffer;
 class QOpenGLShaderProgram;
 class QMatrix4x4;
 class QObject;
 class RenderEngine;
 class Mesh;
 
+///
 /// \brief The Drawable class
 /// \author Andre Furchner
+///
 class Drawable
 {
 public:
+
+    ///
+    /// \brief Create a new Drawable
+    /// \param engine RenderEngine Enviroment
+    /// \param transform Transformation to parent
+    ///
     Drawable(RenderEngine *engine, QMatrix4x4 *transform);
 
+    ///
+    /// \brief Draw the Drawable
+    /// \param transform The SceneGraph transformation
+    ///
     void Draw(QMatrix4x4* transform);
 
+    ///
+    /// \brief Complete the Model
+    ///
     void Build();
 
+    ///
+    /// \brief Set the Mesh that is used to draw the Drawable
+    /// \param mesh The Mesh
+    ///
     void SetMesh(Mesh *mesh);
+
+    ///
+    /// \brief Set the Shader that is used to draw
+    /// \param shader The Shader
+    ///
     void SetShader(QOpenGLShaderProgram *shader);
+
+    ///
+    /// \brief Set the Model Matrix
+    /// \param matrix The Model Matrix
+    ///
     void SetModelMatrix(QMatrix4x4 *matrix);
+
+    ///
+    /// \brief Set the TransformMatrix to his parent
+    /// \param matrix The transform from parant to this Drawable
+    ///
     void SetTransformMatrix(QMatrix4x4 *matrix);
+
+    ///
+    /// \brief Add a Child to this Drawable with an transform offset
+    /// \param child The Child
+    /// \param transform The transform matrix to this Drawable
+    ///
     void AddChild(Drawable *child, QMatrix4x4 *transform);
+
+    ///
+    /// \brief Set the Color
+    /// \param color The Color
+    ///
     void SetColor(QVector4D color);
+
+    ///
+    /// \brief Get the Child that is on the specific index
+    /// \param index Child Index
+    /// \return
+    ///
     Drawable *GetChild(int index);
+
+    ///
+    /// \brief Get the Number of Child that ne Drawable has
+    /// \return Number of Childs
+    ///
     int GetChildCount();
 
+    ///
+    /// \brief Get the TransformMatrix to his parent
+    /// \return The Transform Matrix
+    ///
     QMatrix4x4 *GetTransformMatrix();
 
 private:
-    Mesh *m_mesh;
-    QVector4D m_color;
+    Mesh *m_mesh;                           ///< Geometry Mesh
+    QVector4D m_color;                      ///< Color for Shader
 
-    QOpenGLShaderProgram *m_shader;
-    RenderEngine *m_engine;
+    QOpenGLShaderProgram *m_shader;         ///< Used Shader to Draw
+    RenderEngine *m_engine;                 ///< The RenderEngine Context
 
-    QMatrix4x4 *m_modelMatrix;
-    QMatrix4x4 *m_transMatrix;
-    short m_indexCount;
+    QMatrix4x4 *m_modelMatrix;              ///< Self Matrix
+    QMatrix4x4 *m_transMatrix;              ///< Transformation to his parent
 
-    std::vector<Drawable*> m_container;
-    int m_childCount;
-
+    std::vector<Drawable*> m_childList;     ///< List of Childs
 };
 
 #endif // DRAWABLE_H
