73c60d548f68f089b18e9919a63742d2da8e7f69
Andr? Furchner <storkner@googlemail.com>
2014-05-28 11:52:23 +0200

    color fix

--- QtQuickGL/src/render/scene/task_scene.cpp
+++ QtQuickGL/src/render/scene/task_scene.cpp
@@ -33,28 +33,28 @@ void TaskScene::Create()
     m_gestell->AddChild(m_schlitten,mSchlitten);
 
     // Verbindung Schlitten zu Punkt
-    m_vsp = m_factory->GenBlock(QVector3D(1,1,150),QVector4D(1.0,0.75,0.0,1.0), ds);
+    m_vsp = m_factory->GenBlock(QVector3D(1,1,150),QVector4D(0.5,0.5,0.5,1.0), ds);
     QMatrix4x4 *mvsp = new QMatrix4x4();
     mvsp->setToIdentity();
     mvsp->translate(0,0,95);
     m_schlitten->AddChild(m_vsp,mvsp);
 
     // Punkt
-    m_punkt = m_factory->GenBlock(QVector3D(4,4,4),QVector4D(1.0,0.75,0.0,1.0), ds);
+    m_punkt = m_factory->GenBlock(QVector3D(4,4,4),QVector4D(1.0,0.25,0.0,1.0), ds);
     QMatrix4x4 *mPunkt = new QMatrix4x4();
     mPunkt->setToIdentity();
     mPunkt->translate(0,0,75);
     m_vsp->AddChild(m_punkt,mPunkt);
 
     // H-Linie
-    m_hl = m_factory->GenBlock(QVector3D(120,1,1),QVector4D(1.0,0.75,0.0,1.0), ds);
+    m_hl = m_factory->GenBlock(QVector3D(120,1,1),QVector4D(0.5,0.5,0.5,1.0), ds);
     QMatrix4x4 *mhl = new QMatrix4x4();
     mhl->setToIdentity();
     mhl->translate(0,0,0);
     m_punkt->AddChild(m_hl,mhl);
 
     // Block1
-    m_block1 = m_factory->GenBlock(QVector3D(30,30,30),QVector4D(1.0,0.0,0.25,1.0), ds);
+    m_block1 = m_factory->GenBlock(QVector3D(30,30,30),QVector4D(1.0,0.75,0.0,1.0), ds);
     QMatrix4x4 *mBlock1 = new QMatrix4x4();
     mBlock1->setToIdentity();
     mBlock1->translate(-60,0,0);
@@ -62,7 +62,7 @@ void TaskScene::Create()
     m_hl->AddChild(m_block1,mBlock1);
 
     // Block2
-    m_block2 = m_factory->GenBlock(QVector3D(30,30,30),QVector4D(1.0,0.0,0.25,1.0), ds);
+    m_block2 = m_factory->GenBlock(QVector3D(30,30,30),QVector4D(1.0,0.75,0.0,1.0), ds);
     QMatrix4x4 *mBlock2 = new QMatrix4x4();
     mBlock2->setToIdentity();
     mBlock2->translate(60,0,0);
