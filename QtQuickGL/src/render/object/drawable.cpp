13538811e8f1b0bc7eb2e24bc430f0245854ef67
Aismaelinc <Aismaelinc@localhost>
2014-06-01 19:08:50 +0200

    header fail

---
+++ QtQuickGL/src/render/object/drawable.cpp
@@ -0,0 +1,148 @@
+#include "drawable.h"
+#include "src/io/console.h"
+#include "src/render/render_engine.h"
+#include "src/render/object/mesh.h"
+#include <iostream>
+#include <cmath>
+
+#include <QOpenGLFunctions>
+#include <QOpenGLBuffer>
+#include <QOpenGLVertexArrayObject>
+#include <QOpenGLShaderProgram>
+#include <QMatrix4x4>
+using namespace std;
+
+QVector4D Drawable::s_idCount = QVector4D(0.0f,0.0f,0.0f,1.0f);
+
+Drawable::Drawable(RenderEngine *engine, QMatrix4x4 *transform)
+{
+    m_shader = NULL;
+    m_mesh = NULL;
+    m_modelMatrix = NULL;
+    m_engine = engine;
+    m_transMatrix = transform;
+
+    // create new id
+    // TODO: make this better
+
+
+
+    m_id=makeNewID();
+    //SetColor(m_id);
+
+    cout<<m_id.x()<<"x "<<m_id.y()<<"y "<<m_id.z()<<"z "<<endl;
+
+}
+
+QVector4D Drawable::makeNewID(){
+    unsigned long i=(unsigned long) this;
+    unsigned long t=1000;
+    float b[2];
+    if(s_idCount.x()<=0.9f)
+    s_idCount.setX((s_idCount.x()+0.3f));
+    else
+    s_idCount.setX(0.0f);
+    b[0] = ((float)(((i/1000000)%t)%100))/t;
+    b[1] = ((float)((i/1000)%t))/t;
+    b[2] = ((float)((i)%t))/t;
+    return QVector4D(b[0],b[1],b[2],1.0f)+s_idCount;
+}
+
+void Drawable::Draw(QMatrix4x4 *transform)
+{
+    // bind my shader to be used
+    m_shader->bind();
+
+    // set my uniform color
+    m_shader->setUniformValue("color", m_color);
+
+    // calculate and set uniform variable modelMatrix
+    QMatrix4x4 sceneMatrix = *transform * *m_transMatrix;
+    m_shader->setUniformValue("modelMatrix", sceneMatrix * *m_modelMatrix);
+    m_shader->setUniformValue("sceneMatrix",sceneMatrix);
+    m_shader->setUniformValue("id", m_id);
+
+    // draw myself
+    m_mesh->Draw();
+    m_shader->release();
+
+    // draw my childs
+    for(unsigned int i = 0; i < m_childList.size(); ++i) {
+        m_childList[i]->Draw(&sceneMatrix);
+    }
+}
+
+void Drawable::Build()
+{
+    // first check for wrong usage
+    if(m_shader == NULL) {
+        Console::WriteError("No Shader assigned for this Drawable!");
+        return;
+    }
+
+    if(m_mesh == NULL) {
+        Console::WriteError("No Mesh is assigned to this Drawable!");
+        return;
+    }
+
+    if(m_modelMatrix == NULL) {
+        Console::WriteError("No ModelMatrix is assigned to this Drawable!");
+        return;
+    }
+
+    // set uniform variable
+    m_shader->setUniformValue("modelMatrix", *m_modelMatrix);
+    m_mesh->BuildVAO(m_engine->GetContext(), m_shader);
+
+}
+
+void Drawable::SetMesh(Mesh *mesh)
+{
+    m_mesh = mesh;
+}
+
+void Drawable::SetShader(QOpenGLShaderProgram *shader)
+{
+    m_shader = shader;
+}
+
+void Drawable::SetModelMatrix(QMatrix4x4 *matrix)
+{
+    m_modelMatrix = matrix;
+}
+
+void Drawable::SetTransformMatrix(QMatrix4x4 *matrix)
+{
+    m_transMatrix = matrix;
+}
+
+void Drawable::AddChild(Drawable *child, QMatrix4x4 *transform)
+{
+    child->m_transMatrix = transform;
+    m_childList.push_back(child);
+}
+
+void Drawable::SetColor(QVector4D color)
+{
+    m_color = color;
+}
+
+Drawable *Drawable::GetChild(int index)
+{
+    return m_childList[index];
+}
+
+int Drawable::GetChildCount()
+{
+    return m_childList.size();
+}
+
+QMatrix4x4 *Drawable::GetTransformMatrix()
+{
+    return m_transMatrix;
+}
+
+QVector4D Drawable::GetID()
+{
+    return m_id;
+}
---
+++ QtQuickGL/src/render/window_gl.cpp
@@ -0,0 +1,111 @@
+#include "window_gl.h"
+#include "src/render/render_engine.h"
+#include "src/io/console.h"
+
+#include <iostream>
+#include <QQuickWindow>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLContext>
+#include <QThread>
+#include <QSurfaceFormat>
+#include <QTimer>
+
+using namespace std;
+
+WindowGL::WindowGL()
+{
+    m_engine = NULL;
+    connect(this, SIGNAL(windowChanged(QQuickWindow*)), this, SLOT(handleWindowChanged(QQuickWindow*)));
+}
+
+void WindowGL::handleWindowChanged(QQuickWindow *win)
+{
+    if (win) {
+        // TODO: This cool windowchange Crashes =(
+        //connect(win, SIGNAL(xChanged(int)), this, SLOT(windowChanged()), Qt::DirectConnection);
+        //connect(win, SIGNAL(yChanged(int)), this, SLOT(windowChanged()), Qt::DirectConnection);
+        // TODO: But this dirty one works
+
+        connect(win, SIGNAL(beforeRendering()), this, SLOT(windowChanged()), Qt::DirectConnection);
+        connect(win, SIGNAL(beforeRendering()), this, SLOT(render()), Qt::DirectConnection);
+
+        connect(win, SIGNAL(beforeSynchronizing()), this, SLOT(sync()), Qt::DirectConnection);
+
+        connect(win, SIGNAL(sceneGraphInitialized()), this, SLOT(initialize()), Qt::DirectConnection);
+
+        // update loop
+        connect(win, SIGNAL(afterRendering()),this, SLOT(update()), Qt::DirectConnection);
+
+        win->setClearBeforeRendering(false);
+    }
+}
+
+void WindowGL::windowChanged() {
+    // qDebug() << "resize";
+    float h = (float)window()->size().height();
+    float w = (float)window()->size().width();
+    m_engine->Resize(w,h);
+}
+
+void WindowGL::initialize()
+{
+    Console::Write("Initialize RenderEngine");
+    m_engine = new RenderEngine(parent()->parent());
+    connect(window()->openglContext(), SIGNAL(aboutToBeDestroyed()), this, SLOT(cleanup()), Qt::DirectConnection);
+}
+
+void WindowGL::update()
+{
+    m_engine->Update();
+    window()->update();
+}
+
+void WindowGL::render()
+{
+    m_engine->Render(true);
+}
+
+void WindowGL::cleanup()
+{
+    delete m_engine;
+}
+
+void WindowGL::scaleView (int delta) {
+    m_engine->scaleView(delta);
+}
+
+void WindowGL::setViewMode(int viewMode)
+{
+    m_engine->setViewMode(viewMode);
+}
+
+void WindowGL::setMousePose(int x, int y)
+{
+    m_engine->setMousePose(x, y);
+}
+
+void WindowGL::rotateView (int dx,int dy) {
+    m_engine->rotateView(dx,dy);
+}
+
+int WindowGL::pickObjectAt(int x, int y) {
+    return m_engine->pickObjectAt(x,y);
+}
+
+void WindowGL::rotateObject(int id,int dx,int dy) {
+    m_engine->rotateObject(id,dx,dy);
+}
+
+void WindowGL::scaleObject(int id,int factor) {
+   //TODO ObjectScaling
+}
+void WindowGL::shiftObject(int id, int dx, int dy, int dz){
+   //TODO ObjectShifting
+}
+
+// Copy the state of the object in the
+// GUI thread into the rendering thread.
+void WindowGL::sync()
+{
+    //TODO Sync Shit!!!
+}
