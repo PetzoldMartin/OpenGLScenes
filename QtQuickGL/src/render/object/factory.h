980102d2beed7599243066f5a702542441612ef5
Andr? Furchner <storkner@googlemail.com>
2014-06-03 11:34:41 +0200

    Added support for colors

---
+++ QtQuickGL/src/render/object/factory.h
@@ -0,0 +1,53 @@
+#ifndef FACTORY_H
+#define FACTORY_H
+
+class Drawable;
+class QOpenGLShaderProgram;
+class QVector4D;
+class QVector3D;
+class RenderEngine;
+class Mesh;
+
+///
+/// \brief The Factory class Produces Drawables
+/// \author André Furchner
+///
+class Factory
+{
+public:
+
+    ///
+    /// \brief Create a new Factory
+    /// \param engine The RenderEngine Context
+    ///
+    Factory(RenderEngine *engine);
+
+    ///
+    /// \brief Generate a new Rectangle
+    /// \param size Size of the Rectangle
+    /// \param color Color for the Rectangle
+    /// \param shader Shader to be used when Draw
+    /// \return A new Drawable that is a Rectangle
+    ///
+    Drawable *GenRectangle(QVector3D size, QVector4D color);
+
+    ///
+    /// \brief Generate a new Block
+    /// \param size Size of the Block
+    /// \param color Color for the Block
+    /// \param shader Shader to be used when Draw
+    /// \return A new Drawable that is a Block
+    ///
+    Drawable *GenBlock(QVector3D size, QVector4D color);
+
+private:
+
+    RenderEngine *m_engine;         ///< The RenderEngine Context
+    Mesh *m_rectangle;              ///< The Default Rectangle Mesh
+    Mesh *m_block;                  ///< The Default Block Mesh
+
+    void createMeshRectangle();
+    void createMeshBlock();
+};
+
+#endif // FACTORY_H
---
+++ QtQuickGL/src/render/window_gl.cpp
@@ -0,0 +1,116 @@
+#include "window_gl.h"
+#include "src/render/render_engine.h"
+#include "src/io/console.h"
+
+#include <iostream>
+#include <QQuickWindow>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLContext>
+#include <QThread>
+#include <QSurfaceFormat>
+#include <QTimer>
+
+using namespace std;
+
+WindowGL::WindowGL()
+{
+    m_engine = NULL;
+    connect(this, SIGNAL(windowChanged(QQuickWindow*)), this, SLOT(handleWindowChanged(QQuickWindow*)));
+}
+
+void WindowGL::handleWindowChanged(QQuickWindow *win)
+{
+    if (win) {
+        // TODO: This cool windowchange Crashes =(
+        //connect(win, SIGNAL(xChanged(int)), this, SLOT(windowChanged()), Qt::DirectConnection);
+        //connect(win, SIGNAL(yChanged(int)), this, SLOT(windowChanged()), Qt::DirectConnection);
+        // TODO: But this dirty one works
+
+        connect(win, SIGNAL(beforeRendering()), this, SLOT(windowChanged()), Qt::DirectConnection);
+        connect(win, SIGNAL(beforeRendering()), this, SLOT(render()), Qt::DirectConnection);
+
+        connect(win, SIGNAL(beforeSynchronizing()), this, SLOT(sync()), Qt::DirectConnection);
+
+        connect(win, SIGNAL(sceneGraphInitialized()), this, SLOT(initialize()), Qt::DirectConnection);
+
+        // update loop
+        connect(win, SIGNAL(afterRendering()),this, SLOT(update()), Qt::DirectConnection);
+
+        win->setClearBeforeRendering(false);
+    }
+}
+
+void WindowGL::windowChanged() {
+    // qDebug() << "resize";
+    float h = (float)window()->size().height();
+    float w = (float)window()->size().width();
+    m_engine->Resize(w,h);
+}
+
+void WindowGL::initialize()
+{
+    Console::Write("Initialize RenderEngine");
+    m_engine = new RenderEngine(parent()->parent());
+    connect(window()->openglContext(), SIGNAL(aboutToBeDestroyed()), this, SLOT(cleanup()), Qt::DirectConnection);
+}
+
+void WindowGL::update()
+{
+    m_engine->Update();
+    window()->update();
+}
+
+void WindowGL::render()
+{
+    m_engine->Render(true);
+}
+
+void WindowGL::cleanup()
+{
+    delete m_engine;
+}
+
+void WindowGL::scaleView (int delta) {
+    m_engine->scaleView(delta);
+}
+
+void WindowGL::setViewMode(int viewMode)
+{
+    m_engine->setViewMode(viewMode);
+}
+
+void WindowGL::setMousePose(int x, int y)
+{
+    m_engine->setMousePose(x, y);
+}
+
+void WindowGL::rotateView (int dx,int dy) {
+    m_engine->rotateView(dx,dy);
+}
+
+int WindowGL::pickObjectAt() {
+    return m_engine->pickObjectAt();
+}
+
+void WindowGL::rotateObject(int id,int dx,int dy) {
+    m_engine->rotateObject(id,dx,dy);
+}
+
+void WindowGL::scaleObject(int id,int factor) {
+   //TODO ObjectScaling
+}
+void WindowGL::shiftObject(int id, int dx, int dy, int dz){
+    //TODO ObjectShifting
+}
+
+void WindowGL::createBlock(float sx, float sy, float sz, float tx, float ty, float tz, float cr, float cg, float cb)
+{
+    m_engine->CreateBlock(QVector3D(sx,sy,sz),QVector3D(tx,ty,tz), QVector4D(cr,cg,cb,1.0));
+}
+
+// Copy the state of the object in the
+// GUI thread into the rendering thread.
+void WindowGL::sync()
+{
+    //TODO Sync Shit!!!
+}
