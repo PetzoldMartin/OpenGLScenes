856ec5d9e029485b8544c9e0f66bc78b5ec14668
Andr? Furchner <storkner@googlemail.com>
2014-06-03 08:55:17 +0200

    Refactoring in Factory Class

--- QtQuickGL/src/render/scene/test_scene.cpp
+++ QtQuickGL/src/render/scene/test_scene.cpp
@@ -14,7 +14,7 @@ TestScene::TestScene(RenderEngine *engine) : Scene(engine)
 
 void TestScene::Create()
 {
-    m_d1 = m_factory->GenBlock(QVector3D(10.0f,10.0f,10.0f),QVector4D(1.0,1.0,1.0,1.0),m_engine->GetShader(QString("basic")));
+    m_d1 = m_factory->GenBlock(QVector3D(10.0f,10.0f,10.0f),QVector4D(1.0,1.0,1.0,1.0));
 
 
     for(int i = 0; i < 10; ++i) {
@@ -22,7 +22,7 @@ void TestScene::Create()
         float g = (float)(qrand() % 255) / 255;
         float b = (float)(qrand() % 255) / 255;
 
-        Drawable* d2 = m_factory->GenRectangle(QVector3D(7.0f,7.0f,1.0f),QVector4D(r,g,b,1.0),m_engine->GetShader(QString("basic")));
+        Drawable* d2 = m_factory->GenRectangle(QVector3D(7.0f,7.0f,1.0f),QVector4D(r,g,b,1.0));
 
         QMatrix4x4 m1;
         m1.setToIdentity();
@@ -30,7 +30,7 @@ void TestScene::Create()
         m1.translate(20.0f, 0.0f);
         m_d1->AddChild(d2,m1);
         for(int j = 0; j < 5; ++j) {
-            Drawable* d3 = m_factory->GenRectangle(QVector3D(2.0f,2.0f,1.0f),QVector4D(r,g,b,1.0),m_engine->GetShader(QString("basic")));
+            Drawable* d3 = m_factory->GenRectangle(QVector3D(2.0f,2.0f,1.0f),QVector4D(r,g,b,1.0));
             QMatrix4x4 m2;
             m2.setToIdentity();
             m2.rotate((float)j * 360.0 / 5.0,0.0f,0.0f,1.0f);
