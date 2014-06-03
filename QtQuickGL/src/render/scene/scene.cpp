02c534d7a7a4adf0d9d51c0666f01cbd0a4ea7b5
Andr? Furchner <storkner@googlemail.com>
2014-06-03 09:00:42 +0200

    Removed no more used TaskQueue

--- QtQuickGL/src/render/scene/scene.cpp
+++ QtQuickGL/src/render/scene/scene.cpp
@@ -22,30 +22,3 @@ void Scene::CreateBlock()
 {
     m_objects.push_back(m_factory->GenBlock(QVector3D(50,50,500),QVector4D(1,1,0,1)));
 }
-
-void Scene::AddSceneTask(SceneTask task)
-{
-    m_tasks.enqueue(task);
-}
-
-void Scene::Update()
-{
-    SceneTask c_task;
-    while(m_tasks.size() != 0) {
-        c_task = m_tasks.dequeue();
-
-        switch (c_task.task) {
-        case Task::Create:
-            // TODO Create a new Object
-            break;
-        case Task::Delete:
-            // TODO Delete Drawable
-            break;
-        case Task::Modify:
-            // TODO Modify
-            break;
-        default:
-            break;
-        }
-    }
-}
