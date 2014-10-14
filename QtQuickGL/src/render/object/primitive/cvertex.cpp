35c94d74b039f27ae30998d22bb48398a531131c
Foxel <storkner@gmail.com>
2014-10-14 13:44:31 +0200

    created methods and fields for CVertex

--- QtQuickGL/src/render/object/primitive/cvertex.cpp
+++ QtQuickGL/src/render/object/primitive/cvertex.cpp
@@ -2,5 +2,17 @@
 
 CVertex::CVertex()
 {
-
+    mPosition.setX(0);
+    mPosition.setY(0);
+    mPosition.setZ(0);
 }
+QVector3D CVertex::position() const
+{
+    return mPosition;
+}
+
+void CVertex::setPosition(const QVector3D &position)
+{
+    mPosition = position;
+}
+
