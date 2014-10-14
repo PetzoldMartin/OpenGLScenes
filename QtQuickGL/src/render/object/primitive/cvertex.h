fa11a64455368a549a6125d92569687415ddc635
Foxel <storkner@gmail.com>
2014-10-14 13:44:31 +0200

    created methods and fields for CVertex

--- QtQuickGL/src/render/object/primitive/cvertex.h
+++ QtQuickGL/src/render/object/primitive/cvertex.h
@@ -1,12 +1,17 @@
 #ifndef CVERTEX_H
 #define CVERTEX_H
 
+#include <QVector3D>
+
 class CVertex
 {
 public:
     CVertex();
+    QVector3D position() const;
+    void setPosition(const QVector3D &position);
 
-
+private:
+    QVector3D mPosition;
 };
 
 #endif // CVERTEX_H
