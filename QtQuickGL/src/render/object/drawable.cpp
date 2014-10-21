018a20e629bcd10808f838bdaedfba7af3c02df3
Aismaelinc <Aismaelinc@localhost>
2014-10-21 12:00:48 +0200

    Bugfixes manipulation

--- QtQuickGL/src/render/object/drawable.cpp
+++ QtQuickGL/src/render/object/drawable.cpp
@@ -24,8 +24,8 @@ Drawable::Drawable(RenderEngine *engine, QMatrix4x4 transform)
     m_engine = engine;
     m_transMatrix = transform;
     m_isSelected = false;
+    yr,zr=0;
     xr=1;
-    yr=1;
     m_transform=QVector3D(0.0,0.0,0.0);
 
     // create new id
@@ -115,10 +115,14 @@ void Drawable::TranslateSelectedRelative(QVector3D transform)
 
     m_transMatrix.rotate(((float)xr), QVector3D(1.0f,0.0f,0.0f));
     m_transMatrix.rotate(((float)yr), QVector3D(0.0f,1.0f,0.0f));
+    m_transMatrix.rotate(((float)zr), QVector3D(0.0f,0.0f,1.0f));
 
 }
 void Drawable::RotateDirect(int deltax,int deltay,int deltaz)
 {   m_modelMatrix=m_manipulateMatrix;
+    xr=deltax;
+    yr=deltay;
+    zr=deltaz;
     m_modelMatrix.rotate(((float)deltax), QVector3D(1.0f,0.0f,0.0f));
     m_modelMatrix.rotate(((float)deltay), QVector3D(0.0f,1.0f,0.0f));
     m_modelMatrix.rotate(((float)deltaz), QVector3D(0.0f,0.0f,1.0f));
@@ -137,7 +141,7 @@ void Drawable::RotateRelative(float angle, QVector3D axis)
     m_transMatrix.rotate(angle, axis);
 }
 void Drawable::RotateSelectedRelative(int deltax, int deltay, int deltaz){
-    xr=deltax;yr=deltay;
+    xr=deltax;yr=deltay;zr=deltaz;
     m_transMatrix=m_manipulateRMatrix;
     m_transMatrix.translate(m_transform);
 
