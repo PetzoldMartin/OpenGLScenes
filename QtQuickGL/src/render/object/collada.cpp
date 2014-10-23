f276ea811c2da74714b9d9577469a8fc19f585c9
Foxel <storkner@gmail.com>
2014-10-23 09:20:38 +0200

    Basic XML Parsing

--- QtQuickGL/src/render/object/collada.cpp
+++ QtQuickGL/src/render/object/collada.cpp
@@ -1,6 +1,49 @@
 #include "collada.h"
 
-Collada::Collada()
+#include <QXmlStreamReader>
+#include <iostream>
+
+using namespace std;
+
+Collada::Collada(const QString fileName)
 {
+
+    // Try open file
+    QFile file(fileName);
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        parse(file);
+    } else {
+        cerr << "Cant open File: " << fileName.toStdString() << endl;
+    }
+
+}
+
+void Collada:: parse(QFile& file) {
+
+    QXmlStreamReader xml(&file);
+
+    while (!xml.atEnd()) {
+        if (xml.readNextStartElement()) {
+            if (xml.name() == "float_array") {
+                readFloatArray();
+            } else if (xml.name() == "accessor") {
+                readStride();
+            } else if (xml.name() == "polylist") {
+                readIndexArray();
+            }
+        }
+    }
+
+}
+
+void Collada::readFloatArray() {
+
+}
+
+void Collada::readStride() {
+
+}
+
+void Collada::readIndexArray() {
 
 }
--- QtQuickGL/src/render/object/collada.h
+++ QtQuickGL/src/render/object/collada.h
@@ -1,12 +1,18 @@
 #ifndef COLLADA_H
 #define COLLADA_H
 
-#include <QXmlStreamReader>
+#include <QString>
+#include <QFile>
 
 class Collada
 {
 public:
-    Collada();
+    Collada(const QString fileName);
+private:
+    void parse(QFile &xml);
+    void readFloatArray();
+    void readIndexArray();
+    void readStride();
 };
 
 #endif // COLLADA_H
