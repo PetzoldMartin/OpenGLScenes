add6d5187eedbe6cef83631100e9da198b036489
Foxel <storkner@gmail.com>
2014-10-28 11:43:07 +0100

    created cylinder

--- QtQuickGL/src/render/scene/snowman_scene.cpp
+++ QtQuickGL/src/render/scene/snowman_scene.cpp
@@ -16,7 +16,8 @@ SnowmanScene::SnowmanScene(RenderEngine *engine)
 void SnowmanScene::Create(){
    Drawable *ground = m_factory->GenBlock(QVector3D(512,512,3),QVector4D(0.9,0.9,0.9,1.0));
 
-   Drawable *test = m_factory->GenCollada("test", QVector3D(64,64,64),QVector4D(1.0,0.0,0.25,1.0));
+
+   Drawable *test = m_factory->GenCollada("cylinder", QVector3D(64,64,64),QVector4D(1.0,0.0,0.25,1.0));
    QMatrix4x4 mat_test;
    mat_test.setToIdentity();
    mat_test.translate(-32,32,32);
